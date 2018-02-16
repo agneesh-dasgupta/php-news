9f55cdd79b6560ecd7726b462616c67333b230c6
Danny Aguirre <daniel.aguirreduran@wustl.edu>
2018-02-15 23:12:33 -0600

    added user stories page

---
+++ userstories.php
@@ -0,0 +1,30 @@
+<?php
+    require 'database.php';
+    
+    session_start();
+    
+    $tempUsername = $_SESSION['user_id'];
+    
+    $stmt = $mysqli->prepare("select storytext from stories where username=?");
+    if(!$stmt){
+        printf("Query Prep Failed: %s\n", $mysqli->error);
+        exit;
+    }   
+    
+    $stmt->bind_param('s', $tempUsername);
+
+    $stmt->execute();
+
+    $stmt->bind_result($storyText);
+    
+    echo "<ul>\n";
+    while($stmt->fetch()){
+        printf("\t<li>%s %s</li>\n",
+		htmlspecialchars($storyText)
+	);
+    }
+    echo "</ul>\n";
+
+    $stmt->close();
+    
+?>
