8bf80a513c18ef819cb4717325695e8c6d1eeb3a
Agneesh Dasgupta <agneeshdasgupta@wustl.edu>
2018-02-15 22:44:07 -0600

    updated with mainpage

--- main_page.php
+++ main_page.php
@@ -6,6 +6,29 @@
            </title> 
         </head>
         <body>
-            Successfully landed!
+            <?php
+ require 'database.php';
+$stmt = $mysqli->prepare("select storytext, link from stories order by story_id");
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
+
+$stmt->execute();
+
+$stmt->bind_result($story_text, $link);
+
+echo "<ul>\n";
+while($stmt->fetch()){
+	printf("\t<li>%s %s</li>\n",
+		htmlspecialchars($story_text),
+		htmlspecialchars($link)
+	);
+}
+echo "</ul>\n";
+
+$stmt->close();
+            ?>
+          
         </body>
     </html>
